9b94e738047f0e4707f53bd11c6af9de0fcd5ff9
darius <dariusjosedelacruz@gmail.com>
2023-09-13 01:14:37 -0400

    fixes auth

--- security/urls.py
+++ security/urls.py
@@ -1,9 +1,11 @@
-from django.urls import re_path
+from django.urls import re_path,include
 from rest_framework.routers import DefaultRouter
 
-from security.views import TestingView
+from security.views import TestingView, ActivityViewSet, NotificationViewSet
 
 router = DefaultRouter()
+router.register(r'activity', ActivityViewSet),
+router.register(r'notification', NotificationViewSet)
 
 urlpatterns = [
     re_path(r'^testing/', TestingView.as_view(), name='testing'),
--- security/views/__init__.py
+++ security/views/__init__.py
@@ -1,1 +1,3 @@
 from .testing_view import TestingView
+from .activity_viewset import ActivityViewSet
+from .notification_viewset import NotificationViewSet
